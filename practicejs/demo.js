7508bed87fae68c757e0dff0cb44b18cead7f2c7
prabhaharncad01 <prabhaharncad01@gmail.com>
2024-01-14 16:00:50 +0530

    added some datas

--- practicejs/demo.js
+++ practicejs/demo.js
@@ -62,14 +62,14 @@ showmenu('nav_toggle', 'nav_menu');
 // active & remove active
 
 const navlink = document.querySelectorAll('.nav_link');
-navlink.forEach((n) => n.classList.remove('active'));
+navlink.forEach(n => n.classList.remove('active'));
 
 function linkAction() {
-    navlink.forEach((n) => n.classList.remove('active'));
+    navlink.forEach(n => n.classList.remove('active'));
     this.classList.add('active');
 
     const navmenu= document.getElementById('nav_menu');
     navmenu.classList.remove('show')
 }
 
-navlink.forEach((n) => n.addEventListener('click', linkAction));
+navlink.forEach(n => n.addEventListener('click', linkAction));
