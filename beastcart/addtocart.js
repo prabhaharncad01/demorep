1beb41f32e0f65f851133c2e7593cd70f6a71dab
prabhaharncad01 <prabhaharncad01@gmail.com>
2024-02-21 00:30:17 +0530

    added js codes

--- beastcart/addtocart.js
+++ beastcart/addtocart.js
@@ -44,11 +44,84 @@ function addDatatohtml(){
             let newproduct = document.createElement('div');
             newproduct.classList.add('item');
             newproduct.innerHTML=
-            ` <img src="new-product/N1-1.png" alt="">
-            <h2>Perfume1</h2>
-            <div class="price">$50</div>
-            <button>Add To Cart</button>`;
+            ` <img src="${product.image}">
+            <h2>${product.name}</h2>
+            <div class="price">$ ${product.price}</div>
+            <button onclick="addcart(${product.id})>Add To Cart</button>`;
             listproducthtml.appendChild(newproduct);
         });
+    }
+}
+
+let listcart = [];
+
+//cookies from datacart
+function checkcart(){
+    var cookievalue = document.cookie
+    .split('; ')
+    .find(row => row.startsWith('listcart='));
+    if(cookievalue){
+        listcart = JSON.parse(cookievalue.split('=')[1]);
+    }
+}
+checkcart();
+
+function addcart($idproduct){
+    let productcopy = JSON.parse(JSON.stringify(products));
+
+    //if this products are not in cart
+  if(!listcart[$idproduct]){
+    let dataproduct = productcopy.filter(
+        product => product.id == $idproduct
+    )[0];
+    //add data product in cart
+    listcart[$idproduct] = dataproduct;
+    listcart[$idproduct].quantity =1;
+  }
+  else{
+    // if this product is already in the cart.
+    //then i just increased the quantity
+    listcart[$idproduct].quantity++;
+  }
+  //saving cart datas in cookies
+  //saving cart datas when comp turned off
+  let timesave = "expires=Thu, 31 Dec 2025 23:59:59 UTC";
+  document.cookie = "listcart="+JSON.stringify(listcart)+"; "+timesave+"; path=/;";
+  addcarttohtml();
+}
+addcarttohtml();
+function addcarttohtml(){
+    //clear data
+
+    let listcarthtml = document.querySelector('.listcart');
+    listcarthtml.innerHTML = '';
+
+    let totalhtml = document.querySelector('.totalquantity');
+
+    let totalquantity = 0;
+    if(listcart){
+        listcart.forEach(product => {
+            if(product){
+                let newcart= document.createElement('div');
+                newcart.classList.add('item');
+                newcart.innerHTML = 
+                `<img src="${product.image}" alt="">
+                <div class="content">
+                    <div class="name">
+                    ${product.name}
+    
+                    </div>
+                    <div class="price">
+                        $ ${product.price}/perfume1
+                    </div>
+                </div>
+                <div class="quantity">
+                    <button>-</button>
+                    <span class="value"> ${product.quantity}</span>
+                    <button>+</button>
+                </div>`;
+                listcarthtml.appendChild(newcart);
+            }
+        })
     }
 }
